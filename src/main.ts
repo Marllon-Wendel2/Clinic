b61f0feba098c35189c9c185b346a204b5aa4b87
Marllon Wendel <m.w.p.ferreira@outlook.com>
2026-07-21 06:39:46 -0300

    feat: :sparkles: iniciando backend

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  void app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
